0ae1ccf9e6f37f844c3514be7889e8d13ec1172f
SabrinaOC <sabriojea@gmail.com>
2020-10-17 08:58:19 +0200

    Ejercicio 4.1

---
+++ src/capitulo1/Bloque1_ej4.java
@@ -0,0 +1,37 @@
+package capitulo1;
+
+import javax.swing.JOptionPane;
+
+public class Bloque1_ej4 {
+
+	public static void main(String[] args) {
+		
+		
+		int num;
+		int suma = 0;
+		
+		String str = JOptionPane.showInputDialog("Introduzca un número entero: ");
+		num = Integer.parseInt(str);
+		suma = suma + num;
+		
+		str = JOptionPane.showInputDialog("Introduzca otro número entero: ");
+		num = Integer.parseInt(str);
+		suma = suma +num;
+		
+		str = JOptionPane.showInputDialog("Introduzca un último número entero: ");
+		num = Integer.parseInt(str);
+		suma = suma + num;
+		
+		System.out.println("La media de los números anteriormente introducidos es: " + suma/3);
+		
+	
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
