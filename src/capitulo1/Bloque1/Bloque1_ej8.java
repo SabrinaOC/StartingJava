f8ce2ef13be6d72eb9dd3c66d356131ee92c0446
SabrinaOC <sabriojea@gmail.com>
2020-10-19 20:18:52 +0200

    Último ejercicio

--- src/capitulo1/Bloque1/Bloque1_ej8.java
+++ src/capitulo1/Bloque1/Bloque1_ej8.java
@@ -1,10 +1,54 @@
 package capitulo1.Bloque1;
 
+import javax.swing.JOptionPane;
+
 public class Bloque1_ej8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int num1;
+		int num2;
+		int num3;
+
+		String str = JOptionPane.showInputDialog("Introduzca un número: ");
+		num1 = Integer.parseInt(str);
+
+		str = JOptionPane.showInputDialog("Introduzca otro número:");
+		num2 = Integer.parseInt(str);
+
+		str = JOptionPane.showInputDialog("Introduzca un número:");
+		num3 = Integer.parseInt(str);
+
+		if (num1 > num2) {
+			if (num1 > num3) {
+				System.out.println("El número más grande es: " + num1);
+			} else {
+				System.out.println("El número más grande es: " + num3);
+			}
+		} else if (num2 > num3) {
+			System.out.println("El número más grande es: " + num2);
+		} else {
+			System.out.println("El número más grande es: " + num3);
+		}
+
+//ahora el menor
+
+		if (num1 < num2) {
+			if (num1 < num3) {
+				System.out.println("El número más pequeño es: " + num1);
+			} else {
+				System.out.println("El número más pequeño es: " + num3);
+			}
+		} else if (num2 < num3) {
+			System.out.println("El número más pequeño es: " + num2);
+		} else
+			System.out.println("El número más pequeño es: " + num3);
+	}
+	
+	
+	
+	
+		
 	}
 
-}
+
