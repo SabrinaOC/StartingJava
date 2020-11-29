50f86f525d2f97f258b152c03c444a1c4c8e1b8a
SabrinaOC <sabriojea@gmail.com>
2020-11-29 19:38:30 +0100

    Primeros ejercicios objetos terminados

---
+++ src/capitulo4/bloque01_PrimerosObjetos/Baloncesto.java
@@ -0,0 +1,121 @@
+package capitulo4.bloque01_PrimerosObjetos;
+
+import java.util.ArrayList;
+
+/**
+ * Sigue los siguientes pasos para resolver este ejercicio:
+
+- Crea un paquete, en el repositorio de tus ejercicios, cuyo nombre aproximadamente sea 
+  "capitulo04.bloque01_PrimerosObjetos".
+- Dentro del paquete crea una clase llamada "CromoBaloncesto". 
+- Dentro de la clase, crea las siguientes propiedades: String nombreJugador, int alturaCm, int pesoKg
+  y float puntosMediosPorpartido.
+- Crea un constructor por defecto (sin argumentos de entrada) y un constructor en el que recibas, 
+  por argumentos de entrada, todas las propiedades de los objetos de esta clase
+- Crea un método "toString()" que devuelva una cadena de caracteres que contenga todos los valores 
+  de las propiedades de los objetos de esta clase.
+- Crea métodos getter y setter de todas las propiedades de los objetos de esta clase.
+- Crea una clase nueva, en la que pongas un método main() que lleve a cabo las siguientes acciones:
+    - Crea 5 objetos de tipo CromoBaloncesto, con valores creados por ti.
+    - Crea una lista cuyos elementos sean de tipo CromoBaloncesto y agrega los cinco objetos creados a esa lista.
+    - Recorre la lista, llamando al método toString() de los objetos que contiene y sacando en pantalla el resultado.
+    - Crea un método, en la clase que contiene el método main(), que reciba la lista de cromos 
+      y la ordene, con el algoritmo de la burbuja, en función de los puntosMediosPorPartido de cada jugador, 
+      en orden ascendente. En el método main comprueba el funcionamiento de ese método, 
+      al que puedes llamar "ordenaJugadores(Lista<CromoBaloncesto> lista)"
+ * @author F541U
+ *
+ */
+
+public class Baloncesto {
+	
+	/**
+	 * Método principal
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+
+		CromoBaloncesto jug01 = new CromoBaloncesto(); //Creamos un nuevo jugador llamando al método constructor
+		jug01.setNombreJugador("Facundo Campazzo"); //Damos valores usando los setters
+		jug01.setAltura(180);
+		jug01.setPesoKg(88);
+		jug01.setPuntosMediosPorpartido((float)14.1);
+				
+		//System.out.println(jug01.toString());
+		
+		CromoBaloncesto jug02 = new CromoBaloncesto();//Realizamos el mismo proceso para los 5 jugadores
+		jug02.setNombreJugador("Ricky Rubio");
+		jug02.setAltura(193);
+		jug02.setPesoKg(86);
+		jug02.setPuntosMediosPorpartido((float)13.0);
+		
+		//System.out.println(jug02.toString());
+		
+		
+		CromoBaloncesto jug03 = new CromoBaloncesto("Pau Gasol", 215, 113, (float)12.3); //Usamos el otro método constructor
+		//System.out.println(jug03.toString());
+		
+		CromoBaloncesto jug04 = new CromoBaloncesto("Kobe Bryant", 198, 96, (float)17.6);
+		//System.out.println(jug04.toString());
+		
+		CromoBaloncesto jug05 = new CromoBaloncesto("Andrés Nocioni", 203, 102, (float)11.4);
+		//System.out.println(jug05.toString());
+		
+		
+		ArrayList<CromoBaloncesto> listaCromos = new ArrayList<CromoBaloncesto>(); //Creamos lista de clase CromoBaloncesto
+		listaCromos.add(jug01); //Añadimos jugadores creados a la lista
+		listaCromos.add(jug02);
+		listaCromos.add(jug03);
+		listaCromos.add(jug04);
+		listaCromos.add(jug05);
+		
+		for (int i = 0; i < listaCromos.size(); i++) { //Recorremos lista para mostrar en pantalla
+			System.out.println(listaCromos.get(i).toString()); //Usamos método toString para imprimir
+		}
+		
+		System.out.println("");
+		
+		ordenarListaCromosPorPuntos(listaCromos); //Llamamos método de ordenación
+		
+
+		
+	}
+	
+	/**
+	 * Método para ordenar lista objetos por burbuja
+	 * @param listaCromos
+	 */
+	
+	public static void ordenarListaCromosPorPuntos (ArrayList<CromoBaloncesto> listaCromos) {
+
+		
+		CromoBaloncesto auxiliar;
+		
+		boolean intercambio = false;
+		
+		do {
+			intercambio = false;
+		for (int i = 0; i < listaCromos.size()-1; i ++) { //Recorremos lista para comparar puntos por partido
+			//de los objetos de la lista comparamos solo los puntos
+			if (listaCromos.get(i).getPuntosMediosPorpartido()>listaCromos.get(i+1).getPuntosMediosPorpartido()) {
+				auxiliar = listaCromos.get(i+1); //guardamos todas la propiedades del objeto en la variable auxiliar
+				listaCromos.add(i+1, listaCromos.get(i)); //Añadimos el valor comparado a la lista en la posición correspondiente
+				//esto nos crea un nuevo elemento que deberemos eliminar en la posición i +2 e i ya que son las posiciones que se cambian 
+				listaCromos.remove(i+2); //y así evitamos que se creen múltiples elementos iguales
+				listaCromos.remove(i);
+				
+				listaCromos.add(i, auxiliar);
+				intercambio = true;
+								
+			} 
+		}
+		} while (intercambio); //Repetimos bucle hasta que no haya intercambios
+		
+		for (int i = 0; i < listaCromos.size(); i++) {
+			System.out.println(listaCromos.get(i).toString()); //Imprimimos en pantalla la lista ordenada
+		}
+	}
+
+}
---
+++ src/capitulo4/bloque01_PrimerosObjetos/CromoBaloncesto.java
@@ -0,0 +1,132 @@
+package capitulo4.bloque01_PrimerosObjetos;
+
+/**
+ * crea una clase llamada "CromoBaloncesto". 
+ *	- Dentro de la clase, crea las siguientes propiedades: String nombreJugador, 
+ *	int alturaCm, int pesoKg y float puntosMediosPorpartido.
+ *	- Crea un constructor por defecto (sin argumentos de entrada) y un constructor 
+ *	en el que recibas, por argumentos de entrada, todas las propiedades 
+ *	de los objetos de esta clase
+ *	- Crea un método "toString()" que devuelva una cadena de caracteres 
+ *	que contenga todos los valores de las propiedades de los objetos de esta clase.
+ *	- Crea métodos getter y setter de todas las propiedades de los objetos de esta clase.
+ * @author F541U
+ *
+ */
+
+public class CromoBaloncesto {
+	
+	private String nombreJugador; //creamos propiedades
+	private int alturaCm, pesoKg;
+	private float puntosMediosPorpartido;
+	
+	/**
+	 * Constructor sin argumentos de entrada
+	 */
+	
+	public CromoBaloncesto () {
+		
+	}
+	
+	/**
+	 * Contructor con argumentos de entrada
+	 * @param nombreJugador
+	 * @param alturaCm
+	 * @param pesoKg
+	 * @param puntosMediosPorpartido
+	 */
+	
+	public CromoBaloncesto (String nombreJugador, int alturaCm, int pesoKg, float puntosMediosPorpartido) {
+		this.nombreJugador = nombreJugador;
+		this.alturaCm = alturaCm;
+		this.pesoKg = pesoKg;
+		this.puntosMediosPorpartido = puntosMediosPorpartido;
+	}
+	
+	/**
+	 * Método to string
+	 */
+	
+	public String toString () {
+		return "El jugador " + nombreJugador +" tiene una altura de " + alturaCm + " y un peso de " + pesoKg 
+				+". Su media de puntos por partido es " +puntosMediosPorpartido;
+	}
+	
+	
+	//  Getters y setters
+	 
+	/**
+	 * Get Nombre
+	 * @return
+	 */
+	
+	public String getNombreJugador () {
+		return nombreJugador;
+		
+	}
+	
+	/**
+	 * Get altura
+	 * @return
+	 */
+	
+	public int getAltura () {
+		return alturaCm;
+	}
+
+	/**
+	 * Get peso
+	 * @return
+	 */
+	
+	public int getPesoKg () {
+		return alturaCm;
+		
+	}
+	
+	/**
+	 * Get media puntos
+	 * @return
+	 */
+	
+	public float getPuntosMediosPorpartido () {
+		return puntosMediosPorpartido;
+	}
+	
+	/**
+	 * Set nombre jugador
+	 */
+	
+	public void setNombreJugador (String nombreJugador) {
+		this.nombreJugador = nombreJugador;
+	}
+	
+	/**
+	 * Set puntos
+	 */
+	
+	public void setAltura (int alturaCm) {
+		this.alturaCm = alturaCm;
+	}
+	
+	/**
+	 * Set peso
+	 */
+	
+	public void setPesoKg (int pesoKg) {
+		this.pesoKg = pesoKg;
+	}
+	
+	/**
+	 * Set media puntos partido
+	 */
+	
+	public void setPuntosMediosPorpartido (float puntosMediosPorpartido) {
+		this.puntosMediosPorpartido = puntosMediosPorpartido;
+	}
+	
+	
+	
+	
+	
+}
