062e9e573b0866257ce9e5f58f0d304eb83179f5
SabrinaOC <sabriojea@gmail.com>
2021-03-14 21:30:46 +0100

    Primeros accesos a datos ejercicio fabricante terminado

--- src/capitulo7_Acceso_a_datos/ejercicio01_programa_modificar_BBDD_fabricantes/Programa_fabricantes_BBDD.java
+++ src/capitulo7_Acceso_a_datos/ejercicio01_programa_modificar_BBDD_fabricantes/Programa_fabricantes_BBDD.java
@@ -49,6 +49,11 @@ public class Programa_fabricantes_BBDD {
 			listarFabricantes();
 			break;
 		case 4:// Modificar un fabricante
+			//primero mostramos los fabricantes para que el usuario pueda elegir cuál modificar
+			listarFabricantes();
+			//llamamos al método para modificar
+			modificarFabricantes();
+			listarFabricantes();
 
 			break;
 		default:
@@ -79,12 +84,12 @@ public class Programa_fabricantes_BBDD {
 			// recibe en forma de objeto
 			// de tipo ResultSet, que puede ser navegado para descubrir todos los registros
 			// obtenidos por la consulta
-			ResultSet rs = s.executeQuery("select id, nombre from fabricante");
+			ResultSet rs = s.executeQuery("select * from fabricante");
 
 			System.out.println("Lista de todos los fabricantes:\n");
 			// Navegaci�n del objeto ResultSet
 			while (rs.next()) {
-				System.out.println(rs.getInt(1) + "\t" + rs.getString(2));
+				System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3));
 			}
 			System.out.println("");
 			// Cierre de los elementos
@@ -166,6 +171,7 @@ public class Programa_fabricantes_BBDD {
 		// Pedimos al usuario el id del fabricante que queremos eliminar
 		System.out.println("Introduzca el id del fabricante que desea eliminar:");
 		int id = utils.Utils.obtenerEnteroPorScanner();
+		
 
 		// Iniciamos conexión para realizar consulta
 		try {
@@ -189,11 +195,7 @@ public class Programa_fabricantes_BBDD {
 			// obtenidos por la consulta
 			int regAfectados = s.executeUpdate("delete from fabricante where id=" + id);
 
-//					// Navegaci�n del objeto ResultSet
-//					while (rs.next()) { 
-//						System.out.println (rs.getInt("id") + " " + rs.getString (2)+ 
-//								" " + rs.getString(3) + " " + rs.getString(4) + 
-//								" " + rs.getString(5)); 
+//					
 //					}
 			// Cierre de los elementos
 
@@ -207,5 +209,56 @@ public class Programa_fabricantes_BBDD {
 			ex.printStackTrace();
 		}
 
+	}
+	
+	public static void modificarFabricantes() {
+		// Pedimos al usuario el id del fabricante que queremos modificar
+			System.out.println("Introduzca el id del fabricante que desea modificar:");
+			int id = utils.Utils.obtenerEnteroPorScanner();
+			
+			// Pedimos al usuario cif
+			System.out.println("Introduzca el nuevo cif:");
+			String cif = utils.Utils.obtenerStringPorScanner();
+			
+			// Pedimos al usuario nombre
+			System.out.println("Introduzca el nuevo nombre:");
+			String nombre = utils.Utils.obtenerStringPorScanner();
+		
+			// Iniciamos conexión para realizar consulta
+			try {
+				// A trav�s de la siguiente l�nea comprobamos si tenemos acceso al driver MySQL,
+				// si no fuera as�
+				// no podemos trabajar con esa BBDD.
+				Class.forName("com.mysql.cj.jdbc.Driver");
+
+				// Necesitamos obtener un acceso a la BBDD, eso se materializa en un objeto de
+				// tipo Connection, al cual
+				// le tenemos que pasar los par�metros de conexi�n.
+				Connection conexion = (Connection) DriverManager
+						.getConnection("jdbc:mysql://localhost/tutorialjavacoches?serverTimezone=UTC", "java", "1234");
+
+				// Para poder ejecutar una consulta necesitamos utilizar un objeto de tipo
+				// Statement
+				Statement s = (Statement) conexion.createStatement();
+				// La ejecuci�n de la consulta se realiza a trav�s del objeto Statement y se
+				// recibe en forma de objeto
+				// de tipo ResultSet, que puede ser navegado para descubrir todos los registros
+				// obtenidos por la consulta
+				int regAfectados = s.executeUpdate("update fabricante set cif='" + cif + "', nombre='" + nombre + "' where id=" + id);
+
+//						
+//						}
+				// Cierre de los elementos
+
+				s.close();
+				conexion.close();
+			} catch (ClassNotFoundException ex) {
+				System.out.println("Imposible acceder al driver Mysql");
+				ex.printStackTrace();
+			} catch (SQLException ex) {
+				System.out.println("Error en la ejecuci�n SQL: " + ex.getMessage());
+				ex.printStackTrace();
+			}
+			
 	}
 }
